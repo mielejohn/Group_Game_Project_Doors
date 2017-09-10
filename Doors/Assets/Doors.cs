688064d31bf2e92f9a238460442290851f1379c3
John_Miele <john_miele@bloomfield.edu>
2017-09-10 00:11:25 -0400

    Hey Dean, I hope you're reading this. So i made a few changes to the project you uploaded the other day. So some of the changes are the introduction of some new lines of code, I am attempting to simplify this by creating a door opening animation instead of having the door update automatically with their rotation. I also modified the assets you used. I understand they were stand in assets but I want to have the transition to a final prototype be seamless in the scale of the objects so please if you could make sure that you are scaling up not scaling down. So in summary I added code and modified your assets.

---
+++ Doors/Assets/Doors.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour 
+{
+	public bool startRotationFlag=false;
+	public Transform target;
+	public float speed;
+
+	//New Code
+	public Animation DoorOpening;
+
+	void Start () 
+	{
+		//New Code
+		if (DoorOpening.isPlaying) {
+			DoorOpening.Stop ();
+		}
+	}
+
+	void Update()
+	{
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		//Old Code
+		/*if(startRotationFlag==true){
+			Debug.Log ("RotationStart");
+			this.transform.Rotate (new Vector3(0,90,0));
+
+			if (target.rotation.y == 90.00) {
+				Debug.Log ("Rotation hits 90");
+				startRotationFlag=false;
+				Debug.Log ("startRotation is set to false");
+
+			}
+		} */
+	}
+
+	void OnMouseUp()
+	{
+		Debug.Log ("OnMouseUp");
+		startRotationFlag=true;
+
+		//New Code
+		DoorOpening.Play ();
+		DoorOpening.Stop ();
+	}
+
+}
+
